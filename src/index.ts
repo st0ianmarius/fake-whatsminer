e91c2689e14ab8621693c06f1406b0dc647adf3a
stoianmarius <stoianmarius@live.com>
2022-08-17 17:59:29 +0300

    Remove unnecessary logs.

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import BtminerServer from './servers/btminer.js';
 import LuciServer from './servers/luci.js';
 import ConfigServer from './servers/config.js';
 
-consola.info('Starting fake WhatsMiner...');
-
 const miner = new Miner(config.miner);
 consola.info(
   `${miner.model} | MAC: ${miner.mac} | API: ${miner.apiVersion} | FW: ${miner.firmwareVersion}`
@@ -17,8 +15,6 @@ if (config.commandsControl.syntheticDelay > 0) {
     `Commands are using a synthetic delay of ${config.commandsControl.syntheticDelay}ms`
   );
 }
-
-consola.info('Starting servers...');
 
 // Btminer interface
 const btminerServer = new BtminerServer(miner, config.servers.btminer);

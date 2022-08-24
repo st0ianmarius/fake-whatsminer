aa8f24e0c5fc91153d169a973e413612cdf32e12
stoianmarius <stoianmarius@live.com>
2022-08-24 11:48:41 +0300

    Fix ESM import in miner-stats file.

--- src/miner-control/miner-stats.ts
+++ src/miner-control/miner-stats.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify';
-import Miner, { PowerMode } from '../miner';
+import Miner, { PowerMode } from '../miner.js';
 import { Type } from '@sinclair/typebox';
 
 const MinerStatsResponse = Type.Object({
